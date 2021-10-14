6caee60c452dab00488e5a582491673025f5d549
julian3493 <julianramosarevalo@gmail.com>
2021-10-14 12:35:27 -0500

    create user and track models

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
+  has_many :tracks
+end

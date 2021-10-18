0d1bdf14de4ea74b1d44e8941acc405830b9eafa
julian3493 <julianramosarevalo@gmail.com>
2021-10-18 00:06:46 -0500

    create current_user_concern and create loggued_in and logout methods in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+
   def create
     user = User.find_by(email: params['user']['email']).try(:authenticate, params['user']['password'])
 
@@ -13,4 +15,24 @@ class SessionsController < ApplicationController
       render json: { status: 401 }
     end
   end
+
+  def loggued_in
+    if @current_user
+      render json: {
+        loggued_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        loggued_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: {
+      status: 200,
+      logged_out:  true
+    }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions ,  only: [:create]
   resources :registrations, only: [:create]
+  delete :logout, to: 'sessions#logout'
+  get :loggued_in, to: 'sessions#loggued_in'
   root to: 'static#home'
 end

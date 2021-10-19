982e45f6ff357f92ccf7b741a8f3257c6c463184
julian3493 <julianramosarevalo@gmail.com>
2021-10-19 11:06:50 -0500

    add track routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions ,  only: [:create]
   resources :registrations, only: [:create]
+  resources :tracks, only: [:index, :create]
   delete :logout, to: 'sessions#logout'
   get :loggued_in, to: 'sessions#loggued_in'
   root to: 'static#home'

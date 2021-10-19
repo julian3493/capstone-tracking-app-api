5c7fe712f33270fc89f431a3ce576b114bc0280e
julian3493 <julianramosarevalo@gmail.com>
2021-10-19 11:51:32 -0500

    change url in cors and session_store initializers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "http://appurl.com"
+    origins "https://julian-comsumption-tracker-app.herokuapp.com/"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end

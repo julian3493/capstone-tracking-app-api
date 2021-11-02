f76f1ce0ecfb131843b9dd4cf8f3f92b3457f6ea
julian3493 <julianramosarevalo@gmail.com>
2021-11-02 17:12:59 -0500

    add validations in track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,8 @@
 class Track < ApplicationRecord
+  validates_presence_of :sport
+  validates_presence_of :day
+  validates_presence_of :distance
+  validates_presence_of :moving_time
+
   belongs_to :user
 end
